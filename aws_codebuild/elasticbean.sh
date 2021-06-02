5438bdf4f0a40f8fac3a9797e9ab92dd29213757
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-06-01 22:09:32 -0300

    Update elasticbean.sh

--- aws_codebuild/elasticbean.sh
+++ aws_codebuild/elasticbean.sh
@@ -20,11 +20,12 @@ docker exec -ti awscli aws iam attach-role-policy --policy-arn arn:aws:iam::aws:
 
 #aws iam get-role --role-name elasticbeanFiapRole
 
-# antes de iniciar, deve ser criada pela console a função "eksFiapWoker" e inserir as políticas "AmazonEKSWorkerNodePolicy, AmazonEKS_CNI_Policy e AmazonEC2ContainerRegistryReadOnly"
-#docker exec -ti awscli aws iam create-role --role-name eksFiapWorker --assume-role-policy-document file://eksNodeRole.json
-#docker exec -ti awscli aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy --role-name eksFiapWorker
-#docker exec -ti awscli aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy --role-name eksFiapWorker
-#docker exec -ti awscli aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly --role-name eksFiapWorker
+echo "Digite seu Primeiro Nome:"
+read NOME
+echo "Digite seu Ultimo Sobrenome:"
+read SOBRENOME
+
+eb create pagamento-env -c $NOME$SOBRENOME
 
 
 #docker exec -ti awscli /bin/sh 
