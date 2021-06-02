a74ef1f8a1276c7bc0a6c63e46fc116d7a7f9f8a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-06-01 22:26:44 -0300

    Update codecommit.sh

--- aws_codebuild/codecommit.sh
+++ aws_codebuild/codecommit.sh
@@ -1,5 +1,5 @@
 echo "Iniciando repositorio do Projeto Pagamento SpringRest:"
-cd ~/environment/pagamento-spring-rest-gof
+cd ~/environment/pagamento
 rm -rf .git
 echo -e '*/target/**' >> .gitignore
 git init
