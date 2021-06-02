33e26398954d729c1305e4af7c49f71bdd8022ff
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-06-02 00:09:33 -0300

    Update run_elasticbean.sh

--- aws_codebuild/run_elasticbean.sh
+++ aws_codebuild/run_elasticbean.sh
@@ -1,6 +1,26 @@
-cd ~
-rm -rf pagamento && rm -rf pagamento-bdd
+#!/bin/bash
+cd ~/enviroment
+DIR="$PWD/pagamento"
+if [ -d "$DIR" ]; then
+  rm -rf pagamento
+fi
+DIRBDD="$PWD/pagamento-bdd"
+if [ -d "$DIR" ]; then
+  rm -rf pagamento-bdd
+fi
 git clone https://github.com/tonanuvem/pagamento.git
 git clone https://github.com/tonanuvem/pagamento-bdd.git
-cd pagamento && sh aws_codebuild/codecommit.sh 
-cd aws_codebuild && sh elasticbean.sh && cd ..
+cd pagamento/aws_codebuild
+sh codecommit.sh
+sh config_credenciais.sh
+sh elasticbean_role.sh
+
+cd ~/enviroment/pagamento
+
+echo "Digite seu Primeiro Nome:" && read NOME
+echo "Digite seu Ultimo Sobrenome:" && read SOBRENOME
+
+aws elasticbeanstalk check-dns-availability --cname-prefix $NOME$SOBRENOME
+
+# configurando elasticbeanstalk
+eb create pagamento-env -c $NOME$SOBRENOME
